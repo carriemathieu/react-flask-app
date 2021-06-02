a01db3428d684bd6f009f8678d96a7325e05ec3e
Carrie Mathieu <carrieburks@gmail.com>
2021-06-01 23:31:38 -0400

    db setup

--- api/api.py
+++ api/api.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.db"
 
-@app.route('/', methods=['GET'])
+@app.route('/api', methods=['GET'])
 def index():
     return {
         'name': 'Hello World'
--- src/Pages/TodoPage.js
+++ src/Pages/TodoPage.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import { Card } from '../Components/Card/card'
 
 export const TodoPage = () => {
+
+    const [todo, setTodo] = useState([])
+
+    // fetches data from API
+    useEffect(() => {
+        fetch('/api').then(response => {
+            if(response.ok){
+                return response.json()
+            }
+        }).then(data => console.log(data))
+    }, [])
+
     return (
         <>
         < Card />
