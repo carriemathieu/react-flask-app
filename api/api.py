bf2b737794f7cc2a12bc959ddffafc7e0c6e8a4b
Carrie Mathieu <carrieburks@gmail.com>
2021-06-01 23:41:28 -0400

    todoo_serializer, index

--- api/api.py
+++ api/api.py
@@ -3,12 +3,25 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.db"
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+
+    def __str__(self):
+        return f'{self.id} {self.content}'
+
+def todo_serializer(todo):
+    return {
+        'id': todo.id,
+        'content': todo.content
+    }
 
 @app.route('/api', methods=['GET'])
 def index():
-    return {
-        'name': 'Hello World'
-    }
+    # * unpacks object into array
+    return jsonify([*map(todo_serializer, Todo.query.all())])
 
 if __name__ == '__main__':
     app.run(debug=True)
