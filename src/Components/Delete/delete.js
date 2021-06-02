4fbb678b72f3f1f7d479994ee588f2998aad586a
Carrie Mathieu <carrieburks@gmail.com>
2021-06-02 01:00:31 -0400

    delete component

---
+++ src/Components/Delete/delete.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+
+export const Delete = ({ id }) => {
+    // gives access to history instance
+    const history = useHistory()
+
+    const deleteTodo = () => {
+        fetch(`/api/${id}`, {
+            method: 'POST',
+            body: JSON.stringify({
+                id: id
+            })
+        }).then(response => response.json())
+        .then(data => {
+            console.log(data)
+            history.push('/')
+        })
+    }
+
+    return (
+        <>
+            <button>Delete</button>
+        </>
+    )
+}
